6254effb00edfab5ef56fc3d85b99ff8cdf5525a
beth182 <bethany.saunders@pgr.reading.ac.uk>
2022-10-25 14:52:26 +0100

    begin model performance plot

---
+++ model_performance/main_model_performance.py
@@ -0,0 +1,32 @@
+from scint_flux.functions import read_calculated_fluxes
+from scint_flux import look_up
+from scint_eval.functions import retrieve_model_fluxes
+
+
+scint_path = 12
+DOY_list = [2016126, 2016123]
+var_list = ['QH']
+time_res = '1min_sa10min'
+
+pair_id = look_up.scint_path_numbers[scint_path]
+
+DOY_dict = {}
+
+for DOY in DOY_list:
+    # read the observations
+    df = read_calculated_fluxes.extract_data(doy_list=[DOY],
+                                             pair_id=pair_id,
+                                             var_list=var_list,
+                                             time_res=time_res)
+
+    DOY_dict[DOY] = {time_res: df}
+
+    # get model sensible heat
+    ukv_data_dict_QH = retrieve_model_fluxes.retrieve_UKV(scint_path, DOY, DOY, variable='H')
+    UKV_df_QH = retrieve_model_fluxes.UKV_df(ukv_data_dict_QH)
+    DOY_dict[DOY] = {'obs': df, 'UKV_QH': UKV_df_QH}
+
+model_performance_plot.plot_difference(DOY_dict)
+print('end')
+
+
---
+++ model_performance/model_performance_funs.py
@@ -0,0 +1,76 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+from scint_flux.functions import read_calculated_fluxes
+
+
+def plot_difference(DOY_dict):
+    assert 2016123 in DOY_dict.keys()
+    assert 2016126 in DOY_dict.keys()
+
+    DOY_df_dict = {}
+
+    for DOY in DOY_dict.keys():
+        # unwrap the data
+        obs = DOY_dict[DOY]['obs']
+        UKV = DOY_dict[DOY]['UKV_QH'].WAverage
+
+        # time average the observations
+        obs_av = read_calculated_fluxes.time_averages_of_obs(obs, 'QH', on_hour=True)
+
+        # combine obs and model
+        df = pd.concat([obs_av, UKV.rename('UKV')], axis=1).dropna()
+
+        # take differences
+        df['diff_1'] = (df.UKV - df.obs_1) / np.abs(df.obs_1)
+        df['diff_5'] = (df.UKV - df.obs_5) / np.abs(df.obs_5)
+        df['diff_10'] = (df.UKV - df.obs_10) / np.abs(df.obs_10)
+        df['diff_60'] = (df.UKV - df.obs_60) / np.abs(df.obs_60)
+
+        # print averages for reference
+        print(' ')
+        print('DOY: ', DOY)
+        print('average diff 1: ', np.average(np.abs(df.diff_1)))
+        print('average diff 5: ', np.average(np.abs(df.diff_5)))
+        print('average diff 10: ', np.average(np.abs(df.diff_10)))
+        print('average diff 60: ', np.average(np.abs(df.diff_60)))
+
+        DOY_df_dict[DOY] = df
+
+    # Make plot
+    plt.figure(figsize=(10, 8))
+    ax = plt.subplot(1, 1, 1)
+
+    # 1 mins
+    plt.plot(DOY_df_dict[2016123].index.hour, DOY_df_dict[2016123].diff_1, marker='x', label='Cloudy 1', color='black',
+             linestyle='dotted')
+    plt.plot(DOY_df_dict[2016126].index.hour, DOY_df_dict[2016126].diff_1, marker='o', label='Clear 1', color='black')
+
+    # 5 mins
+    plt.plot(DOY_df_dict[2016123].index.hour, DOY_df_dict[2016123].diff_5, marker='x', label='Cloudy 5', color='green',
+             linestyle='dotted')
+    plt.plot(DOY_df_dict[2016126].index.hour, DOY_df_dict[2016126].diff_5, marker='o', label='Clear 5', color='green')
+
+    # 10 mins
+    plt.plot(DOY_df_dict[2016123].index.hour, DOY_df_dict[2016123].diff_10, marker='x', label='Cloudy 10', color='red',
+             linestyle='dotted')
+    plt.plot(DOY_df_dict[2016126].index.hour, DOY_df_dict[2016126].diff_10, marker='o', label='Clear 10', color='red')
+
+    # 60 mins
+    plt.plot(DOY_df_dict[2016123].index.hour, DOY_df_dict[2016123].diff_60, marker='x', label='Cloudy 60',
+             color='purple', linestyle='dotted')
+    plt.plot(DOY_df_dict[2016126].index.hour, DOY_df_dict[2016126].diff_60, marker='o', label='Clear 60',
+             color='purple')
+
+    # plt.gcf().autofmt_xdate()
+    # ax.xaxis.set_major_formatter(DateFormatter('%H'))
+
+    plt.ylabel('(UKV-$Q_{H}^{surf}$ - LAS-$Q_{H}$) / LAS-$Q_{H}$')
+    plt.xlabel('Time (h, UTC)')
+
+    plt.axhline(y=0, color='k', linestyle='-', linewidth=0.8)
+
+    plt.legend(fontsize=15)
+
+    print('end')
