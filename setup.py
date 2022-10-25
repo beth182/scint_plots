56d0642713241c991fb4c5e3c34c11d41672e484
beth182 <bethany.saunders@pgr.reading.ac.uk>
2022-10-25 14:53:28 +0100

    add setup

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='scint_plots',
+    url='https://github.com/beth182/scint_plots',
+    author='Beth Saunders',
+    author_email='bethany.saunders@pgr.reading.ac.uk',
+    packages=[],
+    install_requires=[
+        'numpy',
+        'scipy',
+        'geopandas',
+        'pandas',
+        'shapely'
+    ],
+    version='0.1',
+    license='MIT',
+    description='todo The description text',
+    long_description='todo The long description text'
+)
