c387838b1183e6635573f2a05d66cdc0078d8c6d
beth182 <bethany.saunders@pgr.reading.ac.uk>
2022-12-20 13:44:21 +0000

    mv sa_lines_funs to scint_fp

--- scint_plots/sa_position_and_lc_fraction/sa_position/main_sa_map.py
+++ scint_plots/sa_position_and_lc_fraction/sa_position/main_sa_map.py
@@ -1,7 +1,7 @@
 import os
 import matplotlib.pyplot as plt
 
-from scint_plots.sa_position_and_lc_fraction.sa_position import sa_map_funs
+import scint_fp.functions.plot_functions.plot_sa_lines.sa_lines_funs as sa_lines
 
 doy_choice = 123
 # ToDo: move these
@@ -9,7 +9,7 @@ sa_dir = 'C:/Users/beths/OneDrive - University of Reading/local_runs_data/fp_out
 
 save_path = os.getcwd().replace('\\', '/') + '/'
 
-file_list = sa_map_funs.find_SA_rasters(sa_main_dir=sa_dir)
-colour_list = sa_map_funs.get_colours(cmap=plt.cm.inferno, file_list=file_list)
-sa_map_funs.plot_sa_lines(file_list=file_list, colour_list=colour_list, doy_choice=doy_choice, save_path=save_path)
+file_list = sa_lines.find_SA_rasters(sa_main_dir=sa_dir)
+colour_list = sa_lines.get_colours(cmap=plt.cm.inferno, file_list=file_list)
+sa_lines.plot_sa_lines(file_list=file_list, colour_list=colour_list, doy_choice=doy_choice, save_path=save_path)
 print('end')
