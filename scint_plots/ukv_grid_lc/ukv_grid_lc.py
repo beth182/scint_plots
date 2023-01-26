85ea069fcba0618ce286abebdf3a03b927c4842e
beth182 <bethany.saunders@pgr.reading.ac.uk>
2023-01-26 16:07:05 +0000

    make colours and legend match obs

--- scint_plots/ukv_grid_lc/ukv_grid_lc.py
+++ scint_plots/ukv_grid_lc/ukv_grid_lc.py
@@ -10,7 +10,7 @@ import os
 from model_eval_tools import look_up
 
 # user choice
-grid_number = 13
+grid_number = 37
 
 # ToDo: move the scripts used to make these csv files over to here from Model_eval
 
@@ -32,15 +32,22 @@ csv_file_path = csv_dir + csv_file_name
 # pandas read site file
 site_df = pd.read_csv(csv_file_path).set_index('GRID')
 
+# combine veg into one type
+site_df['Veg'] = site_df['broadleaf'] + site_df['needleleaf'] + site_df['C3'] + site_df['C4'] + site_df['shrubs']
+
+# drop the cols that make up veg
+site_df = site_df.drop(['broadleaf', 'needleleaf', 'C3', 'C4', 'shrubs', 'ice'], axis=1)
+
+# re-order columns so veg is before impervious
+site_df = site_df[['lake', 'Veg', 'soil', 'canyon', 'roof']]
+
 # pandas df for just the target grid from letter
 # grid_df = site_df.iloc[np.where(site_df.index == grid_letter)[0]]
 grid_df = site_df.loc[grid_letter]
 
 # plot
-series_labels = ['broadleaf tree', 'needleleaf tree', 'Grass', 'C4 grass', 'Shrub', 'lake', 'Soil', 'ice', 'Impervious',
-                 'Building']
-colors = ['teal', 'darkgreen', 'lawngreen', 'coral', 'olive', 'deepskyblue', 'sienna', 'lightsteelblue', 'silver',
-          'dimgrey']
+series_labels = ['Water', 'Vegetation', 'Soil', 'Impervious', 'Building']
+colors = [[(0/256, 191/256, 255/256)], [(202/256, 255/256, 112/256)], 'sienna', [(211/256, 211/256, 211/256)], [(105/256, 105/256, 105/256)]]
 
 f, ax = plt.subplots(figsize=(10, 10))
 
@@ -54,13 +61,8 @@ cum_size = np.zeros(ny)
 
 for i, row_data in enumerate(data):
 
-    if row_data < 0.0000001:
-        leg_label = None
-    else:
-        leg_label = series_labels[i]
-
     axes.append(ax.bar(ind, row_data, bottom=cum_size, color=colors[i], width=1.0,
-                       label=leg_label))
+                       label=series_labels[i]))
 
     cum_size += row_data
 
