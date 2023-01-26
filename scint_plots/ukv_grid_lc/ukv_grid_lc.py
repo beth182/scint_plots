d04a15c7a18ee43eb8288a93aa2a6b8095f042ee
beth182 <bethany.saunders@pgr.reading.ac.uk>
2023-01-26 15:46:38 +0000

    Plot stacked bar of UKV grids

---
+++ scint_plots/ukv_grid_lc/ukv_grid_lc.py
@@ -0,0 +1,80 @@
+# Beth Saunders 26/01/2023
+# script to plot stacked bar of UKV grid landcover fractions
+
+# imports
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+from model_eval_tools import look_up
+
+# user choice
+grid_number = 13
+
+# ToDo: move the scripts used to make these csv files over to here from Model_eval
+
+# save path for plot
+save_path = os.getcwd().replace('\\', '/') + '/'
+
+# existing csv file location
+# ToDo: move to here
+csv_dir = 'C:/Users/beths/OneDrive - University of Reading/landuse/site grids/10-tile landuse (Maggie_new)/landuse_csv_files/'
+
+# use lookup to know which grid number is which site and letter in CSV
+# ToDo: save site + letter to number reference locally
+grid_name = look_up.grid_dict_lc[grid_number][0]
+site_name, grid_letter = grid_name.split(' ')
+
+csv_file_name = site_name + '_10T.csv'
+csv_file_path = csv_dir + csv_file_name
+
+# pandas read site file
+site_df = pd.read_csv(csv_file_path).set_index('GRID')
+
+# pandas df for just the target grid from letter
+# grid_df = site_df.iloc[np.where(site_df.index == grid_letter)[0]]
+grid_df = site_df.loc[grid_letter]
+
+# plot
+series_labels = ['broadleaf tree', 'needleleaf tree', 'Grass', 'C4 grass', 'Shrub', 'lake', 'Soil', 'ice', 'Impervious',
+                 'Building']
+colors = ['teal', 'darkgreen', 'lawngreen', 'coral', 'olive', 'deepskyblue', 'sienna', 'lightsteelblue', 'silver',
+          'dimgrey']
+
+f, ax = plt.subplots(figsize=(10, 10))
+
+data = list(grid_df.values)
+ny = len(data)
+data = np.array(data)
+
+ind = list(range(ny))
+axes = []
+cum_size = np.zeros(ny)
+
+for i, row_data in enumerate(data):
+
+    if row_data < 0.0000001:
+        leg_label = None
+    else:
+        leg_label = series_labels[i]
+
+    axes.append(ax.bar(ind, row_data, bottom=cum_size, color=colors[i], width=1.0,
+                       label=leg_label))
+
+    cum_size += row_data
+
+    ax.axes.get_xaxis().set_visible(False)
+
+    xbuffer = 0
+    ax.set_xlim(-xbuffer, len(ind) - 1 + xbuffer)
+    ax.set_ylim(0, 1)
+
+    ax.axes.get_yaxis().set_visible(False)
+
+# plt.legend(bbox_to_anchor=(1.0, 0.5))
+
+# save plot
+# plt.show()
+plt.savefig(save_path + str(grid_number) + '_ukv_lc.png', bbox_inches='tight', dpi=300)
+print('end')
