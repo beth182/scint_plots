73f0192bdc2cf64d5da5c8e17a033ce5420842aa
beth182 <bethany.saunders@pgr.reading.ac.uk>
2023-02-01 17:29:03 +0000

    add linear regression stats

--- scint_plots/path_comparison/path_scatter.py
+++ scint_plots/path_comparison/path_scatter.py
@@ -7,6 +7,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import os
+from scipy import stats
 
 mpl.rcParams.update({'font.size': 15})
 
@@ -40,6 +41,15 @@ def read_all_scint_data():
     return df_combine
 
 
+def rmse(obs, mod):
+    # ROOT MEAN SQUARE ERROR FUNCTION
+    differencesrmse = mod - obs
+    differences_squared = differencesrmse ** 2
+    mean_of_differences_squared = differences_squared.mean()
+    rmse_val = np.sqrt(mean_of_differences_squared)
+    return rmse_val
+
+
 def scatter_paths(df_combine, save_path):
     """
 
@@ -67,7 +77,29 @@ def scatter_paths(df_combine, save_path):
     df_weekend = df_combine.iloc[weekend_index]
     df_weekday = df_combine.iloc[weekday_index]
 
+    # limits of axis
+    ax_max = df_combine.max().max() + 10
+    ax_min = 0
+
     # BCT_IMU VS IMU_BTT
+
+    # stats
+    # all df
+    all_df_0 = df_combine[['QH_13', 'QH_12']].dropna()
+    gradient0, intercept0, r_value0, p_value0, std_err0 = stats.linregress(all_df_0.QH_13, all_df_0.QH_12)
+    x1_all = np.linspace(ax_min, ax_max, 500)
+    y10 = gradient0 * x1_all + intercept0
+    ax[0].plot(x1_all, y10, linestyle='--', color='grey', zorder=4)
+
+    # text
+    line_string_0 = 'y = ' + str(round(gradient0, 2)) + 'x + ' + str(round(intercept0, 2))
+    r_squ_0 = r_value0 ** 2
+    rmse_0 = rmse(all_df_0.QH_13, all_df_0.QH_12)
+
+    label_string_0 = line_string_0 + '\n' + '$r^{2}$: ' + str(round(r_squ_0, 2)) + '\n' + 'RMSE: ' + str(round(rmse_0, 2))
+    ax[0].text(.01, .99, label_string_0, ha='left', va='top', transform=ax[0].transAxes, color='grey', fontsize=14)
+
+
     # weekday
     weekday_label_0 = 'Weekday\nhours: ' + str(len(df_weekday[['QH_13', 'QH_12']].dropna())) + '\ndays: ' + str(len(
         [group[1] for group in
@@ -89,6 +121,22 @@ def scatter_paths(df_combine, save_path):
     ax[0].xaxis.label.set_color('green')
 
     # BCT_IMU VS BTT_BCT
+
+    # stats
+    # all df
+    all_df_1 = df_combine[['QH_11', 'QH_12']].dropna()
+    gradient1, intercept1, r_value1, p_value1, std_err1 = stats.linregress(all_df_1.QH_11, all_df_1.QH_12)
+    y11 = gradient1 * x1_all + intercept1
+    ax[1].plot(x1_all, y11, linestyle='--', color='grey', zorder=4)
+
+    # text
+    line_string_1 = 'y = ' + str(round(gradient1, 2)) + 'x + ' + str(round(intercept1, 2))
+    r_squ_1 = r_value1 ** 2
+    rmse_1 = rmse(all_df_1.QH_11, all_df_1.QH_12)
+
+    label_string_1 = line_string_1 + '\n' + '$r^{2}$: ' + str(round(r_squ_1, 2)) + '\n' + 'RMSE: ' + str(round(rmse_1, 2))
+    ax[1].text(.01, .99, label_string_1, ha='left', va='top', transform=ax[1].transAxes, color='grey', fontsize=14)
+
     # weekday
     weekday_label_1 = 'Weekday\nhours: ' + str(len(df_weekday[['QH_11', 'QH_12']].dropna())) + '\ndays: ' + str(len(
         [group[1] for group in
@@ -112,6 +160,22 @@ def scatter_paths(df_combine, save_path):
     ax[1].xaxis.label.set_color('blue')
 
     # BCT_IMU VS SCT_SWT
+
+    # stats
+    # all df
+    all_df_2 = df_combine[['QH_15', 'QH_12']].dropna()
+    gradient2, intercept2, r_value2, p_value2, std_err2 = stats.linregress(all_df_2.QH_15, all_df_2.QH_12)
+    y12 = gradient2 * x1_all + intercept2
+    ax[2].plot(x1_all, y12, linestyle='--', color='grey', zorder=4)
+
+    # text
+    line_string_2 = 'y = ' + str(round(gradient2, 2)) + 'x + ' + str(round(intercept2, 2))
+    r_squ_2 = r_value2 ** 2
+    rmse_2 = rmse(all_df_2.QH_15, all_df_2.QH_12)
+
+    label_string_2 = line_string_2 + '\n' + '$r^{2}$: ' + str(round(r_squ_2, 2)) + '\n' + 'RMSE: ' + str(round(rmse_2, 2))
+    ax[2].text(.01, .99, label_string_2, ha='left', va='top', transform=ax[2].transAxes, color='grey', fontsize=14)
+
     # weekday
     weekday_label_2 = 'Weekday\nhours: ' + str(len(df_weekday[['QH_15', 'QH_12']].dropna())) + '\ndays: ' + str(len(
         [group[1] for group in
@@ -144,21 +208,17 @@ def scatter_paths(df_combine, save_path):
     ax[2].spines['left'].set_color('red')
     ax[2].tick_params(axis='y', colors='red')
 
-    ax_x_max = df_combine.max().max() + 10
-    ax_y_max = df_combine.QH_12.max().max() + 10
+    ax[0].set_xlim(ax_min, ax_max)
+    ax[1].set_xlim(ax_min, ax_max)
+    ax[2].set_xlim(ax_min, ax_max)
 
-    ax_min = 0
-    ax[0].set_xlim(ax_min, ax_x_max)
-    ax[1].set_xlim(ax_min, ax_x_max)
-    ax[2].set_xlim(ax_min, ax_x_max)
+    ax[0].set_ylim(ax_min, ax_max)
+    ax[1].set_ylim(ax_min, ax_max)
+    ax[2].set_ylim(ax_min, ax_max)
 
-    ax[0].set_ylim(ax_min, ax_y_max)
-    ax[1].set_ylim(ax_min, ax_y_max)
-    ax[2].set_ylim(ax_min, ax_y_max)
-
-    ax[0].plot((ax_min, ax_y_max), (ax_min, ax_y_max), color='k', zorder=1)
-    ax[1].plot((ax_min, ax_y_max), (ax_min, ax_y_max), color='k', zorder=1)
-    ax[2].plot((ax_min, ax_y_max), (ax_min, ax_y_max), color='k', zorder=1)
+    ax[0].plot((ax_min, ax_max), (ax_min, ax_max), color='k', zorder=1)
+    ax[1].plot((ax_min, ax_max), (ax_min, ax_max), color='k', zorder=1)
+    ax[2].plot((ax_min, ax_max), (ax_min, ax_max), color='k', zorder=1)
 
     cax = ax[3]
     cbar = fig.colorbar(mappable=ye, cax=cax, format='%.0f')
