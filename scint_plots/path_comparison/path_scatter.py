a6f850fbb348e4bfa2b0e29ee868e083d1daeae5
beth182 <bethany.saunders@pgr.reading.ac.uk>
2023-02-06 16:21:58 +0000

    colour bar is season

--- scint_plots/path_comparison/path_scatter.py
+++ scint_plots/path_comparison/path_scatter.py
@@ -8,8 +8,48 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 import os
 from scipy import stats
+import matplotlib.cm as cm
+import matplotlib.colors as mcolors
 
 mpl.rcParams.update({'font.size': 15})
+
+
+def colorbar_index(ncolors, cmap, ticklabels, cax, title=''):
+    # https://stackoverflow.com/questions/18704353/correcting-matplotlib-colorbar-ticks
+    cmap = cmap_discretize(cmap, ncolors)
+    mappable = cm.ScalarMappable(cmap=cmap)
+    mappable.set_array([])
+    mappable.set_clim(-0.5, ncolors + 0.5)
+    colorbar = plt.colorbar(mappable, cax=cax)
+    colorbar.set_ticks(np.linspace(0, ncolors, ncolors))
+    colorbar.set_ticklabels(ticklabels)
+    colorbar.ax.set_title(title)
+
+
+def cmap_discretize(cmap, N):
+    # https://stackoverflow.com/questions/18704353/correcting-matplotlib-colorbar-ticks
+    """Return a discrete colormap from the continuous colormap cmap.
+
+        cmap: colormap instance, eg. cm.jet.
+        N: number of colors.
+
+    Example
+        x = resize(arange(100), (5,100))
+        djet = cmap_discretize(cm.jet, 5)
+        imshow(x, cmap=djet)
+    """
+
+    if type(cmap) == str:
+        cmap = plt.get_cmap(cmap)
+    colors_i = np.concatenate((np.linspace(0, 1., N), (0., 0., 0., 0.)))
+    colors_rgba = cmap(colors_i)
+    indices = np.linspace(0, 1., N + 1)
+    cdict = {}
+    for ki, key in enumerate(('red', 'green', 'blue')):
+        cdict[key] = [(indices[i], colors_rgba[i - 1, ki], colors_rgba[i, ki])
+                      for i in range(N + 1)]
+    # Return colormap object.
+    return mcolors.LinearSegmentedColormap(cmap.name + "_%d" % N, cdict, 1024)
 
 
 def read_all_scint_data():
@@ -56,16 +96,14 @@ def scatter_paths(df_combine, save_path):
     """
     fig, ax = plt.subplots(1, 4, figsize=(16, 6), gridspec_kw={'width_ratios': [1, 1, 1, 0.1]})
 
-    cmap = mpl.cm.get_cmap('viridis')
+    # cmap = mpl.colors.ListedColormap(["blue", "forestgreen", "goldenrod", "red"])
+    cmap = mpl.colors.ListedColormap(["midnightblue", "cadetblue", "orange", "firebrick"])
+    norm = mpl.colors.BoundaryNorm(np.arange(1, 6), cmap.N)
 
-    doys = df_combine.index.strftime('%j').astype(int)
-    earliest_time = doys.min()
-    latest_time = doys.max()
-    bounds = np.linspace(earliest_time, latest_time, 30)
-    norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
+    # get season number as df col
+    df_combine['season'] = df_combine.index.month % 12 // 3 + 1
 
     # weekday or weekend?
-
     df_combine['weekday'] = df_combine.index.weekday
 
     weekend_index = np.where(df_combine.weekday > 5)[0]
@@ -79,14 +117,14 @@ def scatter_paths(df_combine, save_path):
     # limits of axis
     ax_max = df_combine.max().max() + 10
     ax_min = 0
+    x1_all = np.linspace(ax_min, ax_max, 500)
 
     # BCT_IMU VS IMU_BTT
-
+    # """
     # stats
     # all df
     all_df_0 = df_combine[['QH_13', 'QH_12']].dropna()
     gradient0, intercept0, r_value0, p_value0, std_err0 = stats.linregress(all_df_0.QH_13, all_df_0.QH_12)
-    x1_all = np.linspace(ax_min, ax_max, 500)
     y10 = gradient0 * x1_all + intercept0
     ax[0].plot(x1_all, y10, linestyle='--', color='grey', zorder=4)
 
@@ -104,14 +142,14 @@ def scatter_paths(df_combine, save_path):
     weekday_label_0 = 'Weekday\nhours: ' + str(len(df_weekday[['QH_13', 'QH_12']].dropna())) + '\ndays: ' + str(len(
         [group[1] for group in
          df_weekday[['QH_13', 'QH_12']].dropna().groupby(df_weekday[['QH_13', 'QH_12']].dropna().index.date)]))
-    ax[0].scatter(df_weekday.QH_13, df_weekday.QH_12, marker='+', c=df_weekday.index.strftime('%j').astype(int),
-                  norm=norm, cmap=cmap, label=weekday_label_0, alpha=0.6, zorder=2)
+    ax[0].scatter(df_weekday.QH_13, df_weekday.QH_12, marker='+', c=df_weekday.season, cmap=cmap, label=weekday_label_0,
+                  alpha=0.6, zorder=2, norm=norm)
     # weekend
     weekend_label_0 = 'Weekend\nhours: ' + str(len(df_weekend[['QH_13', 'QH_12']].dropna())) + '\ndays: ' + str(len(
         [group[1] for group in
          df_weekend[['QH_13', 'QH_12']].dropna().groupby(df_weekend[['QH_13', 'QH_12']].dropna().index.date)]))
-    ax[0].scatter(df_weekend.QH_13, df_weekend.QH_12, marker='.', c=df_weekend.index.strftime('%j').astype(int),
-                  norm=norm, cmap=cmap, label=weekend_label_0, zorder=3)
+    ax[0].scatter(df_weekend.QH_13, df_weekend.QH_12, marker='.', c=df_weekend.season, cmap=cmap, label=weekend_label_0,
+                  zorder=3, norm=norm)
 
     ax[0].legend(loc="lower right", frameon=False, markerscale=2, handletextpad=0.1, prop={'size': 13})
     leg0 = ax[0].get_legend()
@@ -123,9 +161,10 @@ def scatter_paths(df_combine, save_path):
     ax[0].spines['bottom'].set_color('green')
     ax[0].tick_params(axis='x', colors='green')
     ax[0].xaxis.label.set_color('green')
+    # """
 
     # BCT_IMU VS BTT_BCT
-
+    # """
     # stats
     # all df
     all_df_1 = df_combine[['QH_11', 'QH_12']].dropna()
@@ -148,14 +187,14 @@ def scatter_paths(df_combine, save_path):
     weekday_label_1 = 'Weekday\nhours: ' + str(len(df_weekday[['QH_11', 'QH_12']].dropna())) + '\ndays: ' + str(len(
         [group[1] for group in
          df_weekday[['QH_11', 'QH_12']].dropna().groupby(df_weekday[['QH_11', 'QH_12']].dropna().index.date)]))
-    ax[1].scatter(df_weekday.QH_11, df_weekday.QH_12, marker='+', c=df_weekday.index.strftime('%j').astype(int),
-                  norm=norm, cmap=cmap, label=weekday_label_1, alpha=0.6, zorder=2)
+    ax[1].scatter(df_weekday.QH_11, df_weekday.QH_12, marker='+', c=df_weekday.season, cmap=cmap, label=weekday_label_1,
+                  alpha=0.6, zorder=2, norm=norm)
     # weekend
     weekend_label_1 = 'Weekend\nhours: ' + str(len(df_weekend[['QH_11', 'QH_12']].dropna())) + '\ndays: ' + str(len(
         [group[1] for group in
          df_weekend[['QH_11', 'QH_12']].dropna().groupby(df_weekend[['QH_11', 'QH_12']].dropna().index.date)]))
-    ye = ax[1].scatter(df_weekend.QH_11, df_weekend.QH_12, marker='.', c=df_weekend.index.strftime('%j').astype(int),
-                       norm=norm, cmap=cmap, label=weekend_label_1, zorder=3)
+    ax[1].scatter(df_weekend.QH_11, df_weekend.QH_12, marker='.', c=df_weekend.season, cmap=cmap,
+                  label=weekend_label_1, zorder=3, norm=norm)
 
     ax[1].legend(loc="lower right", frameon=False, markerscale=2, handletextpad=0.1, prop={'size': 13})
     leg1 = ax[1].get_legend()
@@ -169,9 +208,10 @@ def scatter_paths(df_combine, save_path):
     ax[1].spines['bottom'].set_color('blue')
     ax[1].tick_params(axis='x', colors='blue')
     ax[1].xaxis.label.set_color('blue')
+    # """
 
     # BCT_IMU VS SCT_SWT
-
+    # """
     # stats
     # all df
     all_df_2 = df_combine[['QH_15', 'QH_12']].dropna()
@@ -194,20 +234,19 @@ def scatter_paths(df_combine, save_path):
     weekday_label_2 = 'Weekday\nhours: ' + str(len(df_weekday[['QH_15', 'QH_12']].dropna())) + '\ndays: ' + str(len(
         [group[1] for group in
          df_weekday[['QH_15', 'QH_12']].dropna().groupby(df_weekday[['QH_15', 'QH_12']].dropna().index.date)]))
-    ax[2].scatter(df_weekday.QH_15, df_weekday.QH_12, marker='+', c=df_weekday.index.strftime('%j').astype(int),
-                  norm=norm, cmap=cmap, label=weekday_label_2, alpha=0.6, zorder=2)
+    ax[2].scatter(df_weekday.QH_15, df_weekday.QH_12, marker='+', c=df_weekday.season, cmap=cmap, label=weekday_label_2,
+                  alpha=0.6, zorder=2, norm=norm)
     # weekend
     weekend_label_2 = 'Weekend\nhours: ' + str(len(df_weekend[['QH_15', 'QH_12']].dropna())) + '\ndays: ' + str(len(
         [group[1] for group in
          df_weekend[['QH_15', 'QH_12']].dropna().groupby(df_weekend[['QH_15', 'QH_12']].dropna().index.date)]))
-    ax[2].scatter(df_weekend.QH_15, df_weekend.QH_12, marker='.', c=df_weekend.index.strftime('%j').astype(int),
-                  norm=norm, cmap=cmap, label=weekend_label_2, zorder=3)
+    ax[2].scatter(df_weekend.QH_15, df_weekend.QH_12, marker='.', c=df_weekend.season, cmap=cmap, label=weekend_label_2,
+                  zorder=3, norm=norm)
 
     ax[2].legend(loc="lower right", frameon=False, markerscale=2, handletextpad=0.1, prop={'size': 13})
     leg2 = ax[2].get_legend()
     leg2.legendHandles[0].set_color('k')
     leg2.legendHandles[1].set_color('k')
-
 
     ax[2].set_xlabel('SCT_SWT $Q_{H}$ (W m$^{-2}$)')
 
@@ -226,6 +265,7 @@ def scatter_paths(df_combine, save_path):
     ax[1].tick_params(axis='y', colors='red')
     ax[2].spines['left'].set_color('red')
     ax[2].tick_params(axis='y', colors='red')
+    # """
 
     ax[0].set_xlim(ax_min, ax_max)
     ax[1].set_xlim(ax_min, ax_max)
@@ -239,14 +279,12 @@ def scatter_paths(df_combine, save_path):
     ax[1].plot((ax_min, ax_max), (ax_min, ax_max), color='k', zorder=1)
     ax[2].plot((ax_min, ax_max), (ax_min, ax_max), color='k', zorder=1)
 
-    cax = ax[3]
-    cbar = fig.colorbar(mappable=ye, cax=cax, format='%.0f')
-    cbar.ax.set_title('DOY')
+    colorbar_index(ncolors=4, cmap=cmap, ticklabels=['DJF', 'MAM', 'JJA', 'SON'], cax=ax[3], title='Season')
 
     fig.tight_layout()
     plt.subplots_adjust(wspace=0.02, hspace=0.02)
 
-    # plt.show()
+    plt.show()
     plt.savefig(save_path + 'scatter.png', bbox_inches='tight', dpi=300)
 
 
