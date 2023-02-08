66b0a43436feb3d6ae2d001613e422b22f3714e4
beth182 <bethany.saunders@pgr.reading.ac.uk>
2023-02-08 19:55:37 +0000

    loop over all paths

--- scint_plots/built_fraction_multiple/main_built_fraction_multiple.py
+++ scint_plots/built_fraction_multiple/main_built_fraction_multiple.py
@@ -9,30 +9,39 @@ from scint_plots.built_fraction_multiple import lc_fraction_funs
 from scint_plots.built_fraction_multiple import plotting_funs
 
 # user choice
-scint_path = 13
+path_list = [15,11]
 
-pair_id = look_up.scint_path_numbers[scint_path]
 save_path = os.getcwd().replace('\\', '/') + '/'
 
-# read in all files
-# read in csv with days
-# DOY_df = pd.read_csv('C:/Users/beths/OneDrive - University of Reading/Paper 2/days_to_be_read_in.csv')
-DOY_df = pd.read_csv('C:/Users/beths/OneDrive - University of Reading/Paper 2/all_days.csv')
-# take only days of the target path
-scint_path_string = 'P' + str(scint_path)
-df_subset = DOY_df.iloc[np.where(DOY_df[scint_path_string] == 1)[0]]
-df_subset['DOY_string'] = df_subset.year.astype(str) + df_subset.DOY.astype(str)
-df_subset['DOY_string'] = df_subset['DOY_string'].astype(int)
-DOY_list = df_subset.DOY_string.to_list()
+path_dict = {}
+for scint_path in path_list:
 
-# ToDo: replace this temp line to take whole dataset
-# DOY_list = DOY_list[:2]
+    pair_id = look_up.scint_path_numbers[scint_path]
 
-lc_df = lc_fraction_funs.read_preprocessed_lc_csv(scint_path=scint_path, DOY_list=DOY_list, save_path=save_path)
-scint_df = lc_fraction_funs.read_preprocessed_scint_csv(scint_path=scint_path, DOY_list=DOY_list, save_path=save_path)
 
-# combine lc fraction csv's and pre-read hourly data csvs
-df = pd.concat([scint_df, lc_df], axis=1)
+    # read in all files
+    # read in csv with days
+    # DOY_df = pd.read_csv('C:/Users/beths/OneDrive - University of Reading/Paper 2/days_to_be_read_in.csv')
+    DOY_df = pd.read_csv('C:/Users/beths/OneDrive - University of Reading/Paper 2/all_days.csv')
+    # take only days of the target path
+    scint_path_string = 'P' + str(scint_path)
+    df_subset = DOY_df.iloc[np.where(DOY_df[scint_path_string] == 1)[0]]
+    df_subset['DOY_string'] = df_subset.year.astype(str) + df_subset.DOY.astype(str)
+    df_subset['DOY_string'] = df_subset['DOY_string'].astype(int)
+    DOY_list = df_subset.DOY_string.to_list()
 
-plotting_funs.plot_built_fraction_3(df, pair_id, save_path)
+
+    lc_df = lc_fraction_funs.read_preprocessed_lc_csv(scint_path=scint_path, DOY_list=DOY_list, save_path=save_path)
+    scint_df = lc_fraction_funs.read_preprocessed_scint_csv(scint_path=scint_path, DOY_list=DOY_list, save_path=save_path)
+
+    # combine lc fraction csv's and pre-read hourly data csvs
+    df = pd.concat([scint_df, lc_df], axis=1)
+
+    # plotting_funs.plot_built_fraction_3(df, pair_id, save_path)
+
+    path_dict[pair_id] = df
+
+plotting_funs.plot_built_fraction_4(path_dict, save_path)
+
+
 print('end')
