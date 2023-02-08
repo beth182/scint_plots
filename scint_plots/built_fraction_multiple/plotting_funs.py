ccfb7d5a4b6f9ffde33f3f61646aa9374c53cf1a
beth182 <bethany.saunders@pgr.reading.ac.uk>
2023-02-08 18:16:55 +0000

    with lines joining days

--- scint_plots/built_fraction_multiple/plotting_funs.py
+++ scint_plots/built_fraction_multiple/plotting_funs.py
@@ -11,7 +11,7 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 mpl.rcParams.update({'font.size': 15})
 
 
-def plot_built_fraction_2(df):
+def plot_built_fraction_3(df):
     """
 
     :param df:
@@ -19,8 +19,86 @@ def plot_built_fraction_2(df):
     """
 
     # subset df where kdown is bellow 100
-    target_df = df.iloc[np.where(df.kdown >=200)[0]][['QH', 'kdown', 'Urban']].dropna()
+    target_df = df.iloc[np.where(df.kdown >= 200)[0]][['QH', 'kdown', 'Urban']].dropna()
 
+    # make sure df is in chronological order
+    target_df = target_df.sort_index()
+
+    # get individual days
+    groups = target_df.groupby([target_df.index.date])
+
+    # smallest DOY
+    smallest_doy = target_df.index.strftime('%j').astype(int).min()
+    # largest DOY
+    largest_doy = target_df.index.strftime('%j').astype(int).max()
+
+    # set up figure
+    cmap = cm.get_cmap('rainbow')
+    bounds = np.linspace(smallest_doy, largest_doy, len(groups) + 1)
+    norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
+
+    smap = mpl.cm.ScalarMappable(norm=norm, cmap=cmap)
+    # list_of_rgba = smap.to_rgba(target_df.index.strftime('%j').astype(int))
+
+    fig, ax = plt.subplots(figsize=(7, 7))
+
+    # ste up colourbar
+    # invisable plot
+    s = ax.scatter(target_df.Urban, target_df.QH / target_df.kdown, c=target_df.index.strftime('%j').astype(int),
+                   cmap=cmap, norm=norm, zorder=0, alpha=0)
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", size="5%", pad=0.1)
+    cbar = fig.colorbar(mappable=s, cax=cax, orientation="vertical", format='%.0f')
+    cbar.set_alpha(1)
+    cbar.draw_all()
+    cax.set_ylabel('DOY', rotation=270, labelpad=20)
+
+    for i, group in groups:
+        # set group's colour
+        colour = smap.to_rgba(int(i.strftime('%j')))
+
+        # set centre point (daily average)
+        av_qh = group.QH.mean()
+        av_kdown = group.kdown.mean()
+        av_built = group.Urban.mean()
+
+        # plot daily average
+        ax.scatter(av_built, av_qh / av_kdown, c=colour, zorder=3, marker='o',
+                   edgecolor='k')
+
+        for index, row in group.iterrows():
+            x = [av_built, row.Urban]
+            y = [av_qh / av_kdown, row.QH / row.kdown]
+            ax.plot(x, y, c=colour, alpha=0.3, zorder=1)
+
+            print('end')
+
+        # ax.plot(group.Urban, group.QH / group.kdown,
+        #            c=colour, zorder=1, alpha=0.5)
+
+        ax.scatter(group.Urban, group.QH / group.kdown,
+                   c=colour, zorder=2, marker='.', alpha=0.8)
+
+    ax.set_xlabel('Built frac')
+    ax.set_ylabel('QH/Kdn')
+
+    plt.show()
+
+    print('end')
+
+    print('end')
+
+
+def plot_built_fraction_2(df):
+    """
+    Scatter of hours AND day averages
+    where data is only taken when kdn is above 200
+    :param df:
+    :return:
+    """
+
+    # subset df where kdown is bellow 100
+    target_df = df.iloc[np.where(df.kdown >= 200)[0]][['QH', 'kdown', 'Urban']].dropna()
 
     # get individual days
     groups = target_df.groupby([target_df.index.date])
@@ -40,7 +118,6 @@ def plot_built_fraction_2(df):
 
     day_kdowns = np.array(kdwn_list)
     day_qhs = np.array(qh_list)
-
 
     fig, ax = plt.subplots(figsize=(7, 7))
 
@@ -68,8 +145,6 @@ def plot_built_fraction_2(df):
     ax.set_ylabel('QH/Kdn')
 
     print('end')
-
-
 
 
 def plot_built_fraction_1(df):
