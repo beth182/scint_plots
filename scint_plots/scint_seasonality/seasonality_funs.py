c78a639a559f5005cc100f0d48ef31b2b1790a41
beth182 <bethany.saunders@pgr.reading.ac.uk>
2023-02-14 10:38:15 +0000

    swap line styles round

--- scint_plots/scint_seasonality/seasonality_funs.py
+++ scint_plots/scint_seasonality/seasonality_funs.py
@@ -33,10 +33,10 @@ def plot_season_one_panel(season_dict, save_path, variable='QH'):
                 IQR_dict = IQR_path_dict[path]
                 pair_id = look_up.scint_path_numbers[int(path.split('_')[-1])]
 
-                ax.plot(IQR_dict['mean'].index, IQR_dict['mean'], color=colour_dict[pair_id])
+                ax.plot(IQR_dict['mean'].index, IQR_dict['mean'], color=colour_dict[pair_id], linestyle='-.')
                 ax.plot(IQR_dict['median'].index, IQR_dict['median'], color=colour_dict[pair_id], linestyle=':')
-                ax.plot(IQR_dict['25'].columns, IQR_dict['25'].iloc[0], color=colour_dict[pair_id], linestyle='--')
-                ax.plot(IQR_dict['75'].columns, IQR_dict['75'].iloc[0], colour_dict[pair_id], linestyle='--')
+                ax.plot(IQR_dict['25'].columns, IQR_dict['25'].iloc[0], color=colour_dict[pair_id], linestyle='--', alpha=0.4)
+                ax.plot(IQR_dict['75'].columns, IQR_dict['75'].iloc[0], colour_dict[pair_id], linestyle='--', alpha=0.4)
                 ax.fill_between(IQR_dict['25'].columns, IQR_dict['25'].iloc[0], IQR_dict['75'].iloc[0],
                                 color=colour_dict[pair_id], alpha=0.1)
 
