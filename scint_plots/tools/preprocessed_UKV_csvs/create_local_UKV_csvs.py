756a2f255a96aa84fbb978745d54544b5818ebd8
beth182 <bethany.saunders@pgr.reading.ac.uk>
2023-02-17 19:28:57 +0000

    hard code if statements to enable selection of a different level

--- scint_plots/tools/preprocessed_UKV_csvs/create_local_UKV_csvs.py
+++ scint_plots/tools/preprocessed_UKV_csvs/create_local_UKV_csvs.py
@@ -11,8 +11,17 @@ from scint_plots.tools.preprocessed_UKV_csvs import UKV_lookup
 from model_eval_tools.retrieve_UKV import retrieve_ukv_vars
 from scint_flux import look_up
 
+########################################################################################################################
+
 # user choices
-scint_path = 15
+scint_path = 12
+target_level = -1
+# target model levels.
+# 0 = closest level to obs median zf
+# 1 is one above
+# -1 is one bellow
+
+########################################################################################################################
 
 # read in all DOYs for the selected path
 # read in csv with days
@@ -25,21 +34,43 @@ df_subset['DOY_string'] = df_subset['DOY_string'].astype(int)
 DOY_list = df_subset.DOY_string.to_list()
 
 # FOR TESTING
-# DOY_list = DOY_list[:2]
+DOY_list = DOY_list[:2]
 
 pair_id = look_up.scint_path_numbers[scint_path]
+
+# set the target height
+if target_level == 0:  # exact model level
+    target_height = UKV_lookup.scint_median_zf[pair_id]
+
+elif target_level == 1:  # going up a model level
+    if pair_id == 'SCT_SWT' or pair_id == 'BCT_IMU':  # going from level 3 to level 4
+        target_height = UKV_lookup.scint_median_zf[pair_id] + 26.666668
+
+    else:  # going from level 4 to 5
+        target_height = UKV_lookup.scint_median_zf[pair_id] + 33.333321
+
+elif target_level == -1:  # going down a model level
+    if pair_id == 'SCT_SWT':  # going from level 3 to 2
+        target_height = UKV_lookup.scint_median_zf[pair_id] - 20.000002
+
+    elif pair_id == 'BCT_IMU':  # going from level 3 to 2 - but for some reason I need to take a bit more for just this case
+        target_height = UKV_lookup.scint_median_zf[pair_id] - 20.000002 - 5
+
+    else:  # going from level 4 to 3
+        target_height = UKV_lookup.scint_median_zf[pair_id] - 26.666668
+else:
+    raise ValueError('Target level has to be -1, 0 or 1.')
 
 DOY_df_list = []
 model_level_heights = []
 
 for DOY in DOY_list:
-
     # get model sensible heat - BL_H
     run_details_BL_H = {'variable': 'BL_H',
                         'run_time': '21Z',
                         'scint_path': scint_path,
                         'grid_number': UKV_lookup.scint_UKV_grid_choices[pair_id],
-                        'target_height': UKV_lookup.scint_median_zf[pair_id]}
+                        'target_height': target_height}
 
     ukv_data_dict_QH = retrieve_ukv_vars.retrieve_UKV(run_choices=run_details_BL_H, DOYstart=DOY, DOYstop=DOY)
     UKV_df_QH = retrieve_ukv_vars.UKV_df(ukv_data_dict_QH)
@@ -53,7 +84,7 @@ for DOY in DOY_list:
                         'run_time': '21Z',
                         'scint_path': scint_path,
                         'grid_number': UKV_lookup.scint_UKV_grid_choices[pair_id],
-                        'target_height': UKV_lookup.scint_median_zf[pair_id]}
+                        'target_height': target_height}
 
     # get model wind speed and direction
     ukv_data_dict_wind = retrieve_ukv_vars.retrieve_UKV(run_choices=run_details_wind, DOYstart=DOY, DOYstop=DOY)
@@ -86,10 +117,10 @@ for DOY in DOY_list:
     DOY_df_list.append(DOY_df)
     print(DOY)
 
-
 assert model_level_heights.count(model_level_heights[0]) == len(model_level_heights)
 
 df_all = pd.concat(DOY_df_list)
 save_path = os.getcwd().replace('\\', '/') + '/UKV_csv_files/'
-df_all.to_csv(save_path + 'grid_' + str(UKV_lookup.scint_UKV_grid_choices[pair_id]) + '_height_' + str(model_level_heights[0]) + '_' + pair_id + '_vals.csv')
+df_all.to_csv(save_path + 'grid_' + str(UKV_lookup.scint_UKV_grid_choices[pair_id]) + '_height_' + str(
+    model_level_heights[0]) + '_' + pair_id + '_vals.csv')
 print('end')
