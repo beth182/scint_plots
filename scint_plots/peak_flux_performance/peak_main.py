8d284189e40746cd2981849e2d866d4dd6ceca61
beth182 <bethany.saunders@pgr.reading.ac.uk>
2023-02-18 18:07:40 +0000

    changing main peak to use the csvs

--- scint_plots/peak_flux_performance/peak_main.py
+++ scint_plots/peak_flux_performance/peak_main.py
@@ -1,64 +1,63 @@
 import pandas as pd
 
-from scint_flux.functions import read_calculated_fluxes
+
 from scint_flux import look_up
 from scint_flux.beth_temp.peak_analysis import peak_analysis
 
+from scint_plots.tools.preprocessed_scint_csvs import read_obs_csvs
+from scint_plots.tools.preprocessed_UKV_csvs import read_UKV_csvs
+from scint_plots.scint_seasonality import seasonality_funs
+
+# User inputs
 scint_path = 12
-
-# DOY_list = [2016126, 2016123]
-DOY_list = [2016134, 2016103]
-
-"""
-# read in csv with days
-DOY_df = pd.read_csv('C:/Users/beths/OneDrive - University of Reading/Paper 2/categorize_days/days_to_be_read_in.csv')
-# take only days of the target path
-scint_path_string = 'P' + str(scint_path)
-df_subset = DOY_df.iloc[np.where(DOY_df[scint_path_string] == 1)[0]]
-df_subset['DOY_string'] = df_subset.year.astype(str) + df_subset.DOY.astype(str)
-df_subset['DOY_string'] = df_subset['DOY_string'].astype(int)
-DOY_list = df_subset.DOY_string.to_list()
-"""
-
 var_list = ['QH', 'kdown']
 
-# time_res = '1min_sa10min'
-# time_res = '1min'
-time_res = '1min_sa10_mins_ending'
+
 
 pair_id = look_up.scint_path_numbers[scint_path]
 
-DOY_dict = {}
+# replace the QH in var list with BL flux
+UKV_var_list = []
+for variable in var_list:
+    if variable == 'QH':
+        ukv_variable = 'BL_H'
+    else:
+        ukv_variable = variable
+    UKV_var_list.append(ukv_variable)
 
-df_list = []
 
-for DOY in DOY_list:
-    # read the observations
-    df = read_calculated_fluxes.extract_data(doy_list=[DOY],
-                                             pair_id=pair_id,
-                                             var_list=var_list,
-                                             time_res=time_res)
 
-    DOY_dict[DOY] = {time_res: df}
 
-    # model data
-    # retrieve BL_H
-    ukv_data_dict_BL_H = retrieve_model_fluxes.retrieve_UKV(scint_path, DOY, DOY, variable='BL_H')
-    UKV_df_BL_H = retrieve_model_fluxes.UKV_df(ukv_data_dict_BL_H)
-    # retrieve kdown
-    ukv_data_dict_kdown = retrieve_model_fluxes.retrieve_UKV(scint_path, DOY, DOY, variable='kdown', sa_analysis=False)
-    UKV_df_kdown = retrieve_model_fluxes.UKV_df(ukv_data_dict_kdown)
 
-    # combine model df
-    UKV_df = pd.concat([UKV_df_BL_H, UKV_df_kdown], axis=1).rename(columns={'13': 'kdown_UKV', 'BL_H_13': 'BL_H_UKV'})
+# read the premade scint data csv files
+df_all = read_obs_csvs.read_all_of_preprocessed_scint_csv(var_list)
 
-    peak_df = peak_analysis.peak_BE(df, UKV_df, pair_id)
+# read the premade UKV data csv files
+df_UKV_all = read_UKV_csvs.read_all_of_preprocessed_UKV_csv(UKV_var_list)
 
-    df_list.append(peak_df)
+# drop columns not the path choice or BCT-IMU
+df = seasonality_funs.drop_unchosen_cols(scint_path, df_all).dropna()
+df_UKV = seasonality_funs.drop_unchosen_cols(scint_path, df_UKV_all).dropna()
 
-    print('end')
 
+# per day
+peak_df = peak_analysis.peak_BE(df, UKV_df, pair_id)
+
+
+
+
+
+
+
+
+
+# all days
 peak_df = pd.concat(df_list)
+
+
+
+
+
 # save csv
 # peak_df.to_csv('C:/Users/beths/OneDrive - University of Reading/Paper 2/categorize_days/peak_analysis/peak_analysis_' + pair_id +'.csv')
 
