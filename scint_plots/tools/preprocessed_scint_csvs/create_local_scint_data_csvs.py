64dc40200a8c993fef25c03037eef914c11c123f
beth182 <bethany.saunders@pgr.reading.ac.uk>
2023-02-20 15:32:31 +0000

    add average into file name for csvs

--- scint_plots/tools/preprocessed_scint_csvs/create_local_scint_data_csvs.py
+++ scint_plots/tools/preprocessed_scint_csvs/create_local_scint_data_csvs.py
@@ -10,7 +10,8 @@ from scint_flux.functions import read_calculated_fluxes
 
 # re-run here if I am needing to re-write obs csv
 def save_df_to_csv(save_path='./',
-                   main_dir='//rdg-home.ad.rdg.ac.uk/research-nfs/basic/micromet/Tier_processing/rv006011/scint_data_testing/data/'):
+                   main_dir='//rdg-home.ad.rdg.ac.uk/research-nfs/basic/micromet/Tier_processing/rv006011/scint_data_testing/data/',
+                   average=60):
     """
 
     :return:
@@ -31,39 +32,39 @@ def save_df_to_csv(save_path='./',
 
         if row.P13 == 1:
             df_13 = read_calculated_fluxes.extract_data([int(row.DOY_string)], pair_id='IMU_BTT',
-                                                        var_list=list_of_vars, average=60, hour_ending=True)
+                                                        var_list=list_of_vars, average=average, hour_ending=True)
             df_dict['13'].append(df_13)
 
         if row.P11 == 1:
             df_11 = read_calculated_fluxes.extract_data([int(row.DOY_string)], pair_id='BTT_BCT',
-                                                        var_list=list_of_vars, average=60, hour_ending=True)
+                                                        var_list=list_of_vars, average=average, hour_ending=True)
             df_dict['11'].append(df_11)
 
         if row.P12 == 1:
             df_12 = read_calculated_fluxes.extract_data([int(row.DOY_string)], pair_id='BCT_IMU',
-                                                        var_list=list_of_vars, average=60, hour_ending=True)
+                                                        var_list=list_of_vars, average=average, hour_ending=True)
             df_dict['12'].append(df_12)
 
         if row.P15 == 1:
             df_15 = read_calculated_fluxes.extract_data([int(row.DOY_string)], pair_id='SCT_SWT',
-                                                        var_list=list_of_vars, average=60, hour_ending=True)
+                                                        var_list=list_of_vars, average=average, hour_ending=True)
             df_dict['15'].append(df_15)
 
     df_combine_13 = pd.concat(df_dict['13'])
-    df_combine_13.to_csv(save_path + 'path_13_vals.csv')
+    df_combine_13.to_csv(save_path + 'path_13_' + str(average) + 'vals.csv')
 
     df_combine_15 = pd.concat(df_dict['15'])
-    df_combine_15.to_csv(save_path + 'path_15_vals.csv')
+    df_combine_15.to_csv(save_path + 'path_15_' + str(average) + 'vals.csv')
 
     df_combine_12 = pd.concat(df_dict['12'])
-    df_combine_12.to_csv(save_path + 'path_12_vals.csv')
+    df_combine_12.to_csv(save_path + 'path_12_' + str(average) + 'vals.csv')
 
     df_combine_11 = pd.concat(df_dict['11'])
-    df_combine_11.to_csv(save_path + 'path_11_vals.csv')
+    df_combine_11.to_csv(save_path + 'path_11_' + str(average) + '_vals.csv')
 
 
 if __name__ == '__main__':
     save_path = os.getcwd().replace('\\', '/') + '/csv_files/'
 
-    save_df_to_csv(save_path)
+    save_df_to_csv(save_path, average=60)
     print('end')
