4540dd550825046595d540b9b13eb3505dea4df3
beth182 <bethany.saunders@pgr.reading.ac.uk>
2023-02-21 21:58:17 +0000

    fix on HR fun - but actually gives the same results

--- scint_plots/tools/eval_stat_funs.py
+++ scint_plots/tools/eval_stat_funs.py
@@ -43,13 +43,14 @@ def variable_hitrate(obs, mod, obs_uncertainty=10):
     # calculate the observation error
     df['obs_error'] = (df.obs / 100) * obs_uncertainty
 
-    df['BE'] = mod - obs
-    df['AE'] = abs(df['BE'])
+    df['max_threshold'] = df['obs'] + df['obs_error']
+    df['min_threshold'] = df['obs'] - df['obs_error']
 
     # set up dataframe with column of zeros
     df['hits'] = np.zeros(len(df))
+
     # change to one for successful hit
-    df.hits[df.AE <= df.obs_error] = 1
+    df.hits[(df['mod'] >= df['min_threshold']) & (df['mod'] <= df['max_threshold'])] = 1
 
     # get hit rate
     hr_variable = df.hits.mean() * 100
