769328ce51b6e00ee6709df5d66e99bd5f88938c
beth182 <bethany.saunders@pgr.reading.ac.uk>
2023-02-21 20:47:27 +0000

    fill in missing vals

--- scint_plots/tools/preprocessed_UKV_csvs/UKV_lookup.py
+++ scint_plots/tools/preprocessed_UKV_csvs/UKV_lookup.py
@@ -7,9 +7,8 @@ scint_median_zf = {'BCT_IMU': 73.6, 'IMU_BTT': 103.3, 'BTT_BCT': 113.5, 'SCT_SWT
 # Dict for model level heigths of closest model level to median zf (0), and one above / bellow (1/-1) rounded to the
 # nearest meter
 # used as a look up to find file names for model offline csv files
-# ToDo: fill in the 0 vals (stand in for unkown vals) once the csv files have been created
-model_level_heights = {'BCT_IMU': {1: 89, 0: 63, -1: 0},
-                       'IMU_BTT': {1: 0, 0: 103, -1: 77},
-                       'BTT_BCT': {1: 0, 0: 103, -1: 77},
-                       'SCT_SWT': {1: 0, 0: 36, -1: 16}
+model_level_heights = {'BCT_IMU': {1: 89, 0: 63, -1: 43},
+                       'IMU_BTT': {1: 137, 0: 103, -1: 77},
+                       'BTT_BCT': {1: 137, 0: 103, -1: 77},
+                       'SCT_SWT': {1: 63, 0: 36, -1: 16}
                        }
