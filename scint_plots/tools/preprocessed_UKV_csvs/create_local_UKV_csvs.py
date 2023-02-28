0eef4eef51bc94f5c7e30677552a210a61e314a9
beth182 <bethany.saunders@pgr.reading.ac.uk>
2023-02-28 14:11:45 +0000

    fill in vals for grids

--- scint_plots/tools/preprocessed_UKV_csvs/create_local_UKV_csvs.py
+++ scint_plots/tools/preprocessed_UKV_csvs/create_local_UKV_csvs.py
@@ -14,8 +14,8 @@ from scint_flux import look_up
 ########################################################################################################################
 
 # user choices
-scint_path = 12
-target_level = 0
+scint_path = 11
+target_level = 1
 # target model levels.
 # 0 = closest level to obs median zf
 # 1 is one above
