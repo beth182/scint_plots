2f2b9907911e339c01aaf537ecab37f51d9d4b09
beth182 <bethany.saunders@pgr.reading.ac.uk>
2023-02-28 15:11:40 +0000

    add grid priority to read model csvs

--- scint_plots/tools/preprocessed_UKV_csvs/read_UKV_csvs.py
+++ scint_plots/tools/preprocessed_UKV_csvs/read_UKV_csvs.py
@@ -7,7 +7,8 @@ from scint_flux import look_up
 
 def read_all_of_preprocessed_UKV_csv(list_of_vars=['BL_H'],
                                      csv_dir='D:/Documents/scint_plots/scint_plots/tools/preprocessed_UKV_csvs/UKV_csv_files/',
-                                     model_level=0):
+                                     model_level=0,
+                                     grid_priority='primary'):
     """
 
     :return:
@@ -16,13 +17,19 @@ def read_all_of_preprocessed_UKV_csv(list_of_vars=['BL_H'],
     # read in locally saved files
     path_list = [11, 12, 13, 15]
 
+    if grid_priority == 'primary':
+        grid_ind = 1
+    else:
+        assert grid_priority == 'secondary'
+        grid_ind = 2
+
     path_df_dict = {}
     for path in path_list:
         pair_id = look_up.scint_path_numbers[path]
 
         model_level_height = UKV_lookup.model_level_heights[pair_id][model_level]
 
-        file_name = 'grid_' + str(UKV_lookup.scint_UKV_grid_choices[pair_id]) + '_height_' + str(model_level_height) + '_' + pair_id + '_vals.csv'
+        file_name = 'grid_' + str(UKV_lookup.scint_UKV_grid_choices[pair_id][grid_ind]) + '_height_' + str(model_level_height) + '_' + pair_id + '_vals.csv'
 
         df = pd.read_csv(csv_dir + file_name)
         df['time'] = pd.to_datetime(df['time'], format='%Y-%m-%d %H:%M:%S')
