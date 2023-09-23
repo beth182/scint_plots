13e130744921109b9aff9529b3a61394647563c4
beth182 <bethany.saunders@pgr.reading.ac.uk>
2023-09-23 17:05:51 +0100

    Update with new UKV grid selection

--- scint_plots/model_performance/main_model_performance.py
+++ scint_plots/model_performance/main_model_performance.py
@@ -1,5 +1,6 @@
 from scint_flux.functions import read_calculated_fluxes
 from scint_flux import look_up
+from scint_plots.tools.preprocessed_UKV_csvs import UKV_lookup
 
 from model_eval_tools.retrieve_UKV import retrieve_ukv_vars
 
@@ -21,8 +22,14 @@ for DOY in DOY_list:
                                              var_list=var_list,
                                              time_res=time_res)
 
+    run_details = {'variable': 'H',
+                   'run_time': '21Z',
+                   'scint_path': scint_path,
+                   'grid_number': UKV_lookup.scint_UKV_grid_choices[pair_id][1],  # this is only used if sa_analysis is set to False
+                   'target_height': df.z_f.mean()}
+
     # get model sensible heat
-    ukv_data_dict_QH = retrieve_ukv_vars.retrieve_UKV(scint_path, DOY, DOY, variable='H')
+    ukv_data_dict_QH = retrieve_ukv_vars.retrieve_UKV(run_choices=run_details, DOYstart=DOY, DOYstop=DOY)
     UKV_df_QH = retrieve_ukv_vars.UKV_df(ukv_data_dict_QH)
     DOY_dict[DOY] = {'obs': df, 'UKV_QH': UKV_df_QH}
 
