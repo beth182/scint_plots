1d6f820ab1df934fc7db915c0a000970e8cd5aa8
beth182 <bethany.saunders@pgr.reading.ac.uk>
2023-09-26 16:51:01 +0100

    UKV stats added

--- scint_plots/P1_stats_table/main_scint_stats.py
+++ scint_plots/P1_stats_table/main_scint_stats.py
@@ -1,7 +1,11 @@
 # imports
+import numpy as np
+
 from scint_flux import look_up
 from scint_flux.functions import read_calculated_fluxes
 from scint_plots.P1_stats_table import scint_stat_funs
+from scint_plots.tools.preprocessed_UKV_csvs import UKV_lookup
+from model_eval_tools.retrieve_UKV import retrieve_ukv_vars
 
 
 # main script for running stat functions of P1: Results table
@@ -9,7 +13,7 @@ from scint_plots.P1_stats_table import scint_stat_funs
 scint_path = 12
 DOY_list = [2016123]
 
-var_list = ['QH', 'kdown']
+var_list = ['QH', 'kdown', 'z_f']
 
 time_res = '1min_sa10min'
 # time_res = '1min'
@@ -23,7 +27,34 @@ for DOY in DOY_list:
                                              var_list=var_list,
                                              time_res=time_res)
 
-
+    # stats of observations
+    """
     scint_stat_funs.stats_of_the_obs_fluxes(df)
-
     print('end')
+    """
+
+    # stats of model
+    # """
+    # retrieve UKV data
+    run_details_H = {'variable': 'H',
+                   'run_time': '21Z',
+                   'scint_path': scint_path,
+                   'grid_number': UKV_lookup.scint_UKV_grid_choices[pair_id][1],  # this is only used if sa_analysis is set to False
+                   'target_height': df.z_f.mean()}
+    run_details_kdown = {'variable': 'kdown',
+                   'run_time': '21Z',
+                   'scint_path': scint_path,
+                   'grid_number': UKV_lookup.scint_UKV_grid_choices[pair_id][1],  # this is only used if sa_analysis is set to False
+                   'target_height': np.nan}
+
+    # get model sensible heat
+    ukv_data_dict_QH = retrieve_ukv_vars.retrieve_UKV(run_choices=run_details_H, DOYstart=DOY, DOYstop=DOY)
+    UKV_df_QH = retrieve_ukv_vars.UKV_df(ukv_data_dict_QH)
+    # get model kdown
+    ukv_data_dict_kdown = retrieve_ukv_vars.retrieve_UKV(run_choices=run_details_kdown, DOYstart=DOY, DOYstop=DOY)
+    UKV_df_kdown = retrieve_ukv_vars.UKV_df(ukv_data_dict_kdown)
+
+    scint_stat_funs.stats_of_model(df, UKV_df_QH, UKV_df_kdown)
+    print('end')
+
+    # """
--- scint_plots/P1_stats_table/scint_stat_funs.py
+++ scint_plots/P1_stats_table/scint_stat_funs.py
@@ -1,7 +1,10 @@
 # IMPORTS
 import numpy as np
+import pandas as pd
+import datetime as dt
 
 from scint_flux.functions import read_calculated_fluxes
+
 
 def stats_of_the_obs_fluxes(df):
     """
@@ -57,7 +60,6 @@ def stats_of_the_obs_fluxes(df):
     print('QH: ', mean_QH)
     print('KDOWN: ', mean_kdown)
 
-
     # standard deviations
     print(' ')
     QH_std = QH.resample('10T', closed='right', label='right').std()
@@ -91,5 +93,146 @@ def stats_of_the_obs_fluxes(df):
     kdown_std_mean = kdown_std.mean()
     print('MEAN KDOWN')
     print(kdown_std_mean)
+
+    print('end')
+
+
+def stats_of_model(df, UKV_df_QH, UKV_df_kdown):
+    # Average the obs
+    # time average the obs
+    QH_avs = read_calculated_fluxes.time_averages_of_obs(df, 'QH', on_hour=True)
+    kdown_avs = read_calculated_fluxes.time_averages_of_obs(df, 'kdown')
+
+    print(' ')
+    print('UKV mean')
+    print('QH')
+    print(UKV_df_QH.WAverage.mean())
+    print('KDOWN')
+    print(UKV_df_kdown.WAverage.mean())
+
+    compare_1_QH = pd.concat([QH_avs.obs_1, UKV_df_QH.WAverage], axis=1).dropna()
+    compare_5_QH = pd.concat([QH_avs.obs_5, UKV_df_QH.WAverage], axis=1).dropna()
+    compare_10_QH = pd.concat([QH_avs.obs_10, UKV_df_QH.WAverage], axis=1).dropna()
+    compare_60_QH = pd.concat([QH_avs.obs_60, UKV_df_QH.WAverage], axis=1).dropna()
+
+    kdown_60 = kdown_avs.obs_60.dropna()
+    kdown_60.index = kdown_60.index - dt.timedelta(minutes=45)
+    kdown_10 = kdown_avs.obs_10.dropna()
+    kdown_10.index = kdown_10.index + dt.timedelta(minutes=5)
+    kdown_5 = kdown_avs.obs_5.dropna()
+    kdown = kdown_avs.obs_1.dropna()
+
+    # find common times - can't do this within the built function - as for the model eval purposes, kdown averages are
+    # different
+    compare_1_kdown = pd.concat([kdown.iloc[np.where([i.minute == 15 for i in kdown.index])[0]], UKV_df_kdown.WAverage],
+                                axis=1).dropna()
+    compare_5_kdown = pd.concat(
+        [kdown_5.iloc[np.where([i.minute == 15 for i in kdown_5.index])[0]], UKV_df_kdown.WAverage], axis=1).dropna()
+    compare_10_kdown = pd.concat(
+        [kdown_10.iloc[np.where([i.minute == 15 for i in kdown_10.index])[0]], UKV_df_kdown.WAverage], axis=1).dropna()
+    compare_60_kdown = pd.concat(
+        [kdown_60.iloc[np.where([i.minute == 15 for i in kdown_60.index])[0]], UKV_df_kdown.WAverage], axis=1).dropna()
+
+    # absolute difference
+    compare_1_QH['abs_diff'] = np.abs(compare_1_QH.obs_1 - compare_1_QH.WAverage)
+    compare_5_QH['abs_diff'] = np.abs(compare_5_QH.obs_5 - compare_5_QH.WAverage)
+    compare_10_QH['abs_diff'] = np.abs(compare_10_QH.obs_10 - compare_10_QH.WAverage)
+    compare_60_QH['abs_diff'] = np.abs(compare_60_QH.obs_60 - compare_60_QH.WAverage)
+
+    compare_1_kdown['abs_diff'] = np.abs(compare_1_kdown.obs_1 - compare_1_kdown.WAverage)
+    compare_5_kdown['abs_diff'] = np.abs(compare_5_kdown.obs_5 - compare_5_kdown.WAverage)
+    compare_10_kdown['abs_diff'] = np.abs(compare_10_kdown.obs_10 - compare_10_kdown.WAverage)
+    compare_60_kdown['abs_diff'] = np.abs(compare_60_kdown.obs_60 - compare_60_kdown.WAverage)
+
+    # difference
+    compare_1_QH['difference'] = compare_1_QH.obs_1 - compare_1_QH.WAverage
+    compare_5_QH['difference'] = compare_5_QH.obs_5 - compare_5_QH.WAverage
+    compare_10_QH['difference'] = compare_10_QH.obs_10 - compare_10_QH.WAverage
+    compare_60_QH['difference'] = compare_60_QH.obs_60 - compare_60_QH.WAverage
+
+    compare_1_kdown['difference'] = compare_1_kdown.obs_1 - compare_1_kdown.WAverage
+    compare_5_kdown['difference'] = compare_5_kdown.obs_5 - compare_5_kdown.WAverage
+    compare_10_kdown['difference'] = compare_10_kdown.obs_10 - compare_10_kdown.WAverage
+    compare_60_kdown['difference'] = compare_60_kdown.obs_60 - compare_60_kdown.WAverage
+
+    # Mean Bias error
+    MBE_QH_1 = compare_1_QH.difference.mean()
+    MBE_QH_5 = compare_5_QH.difference.mean()
+    MBE_QH_10 = compare_10_QH.difference.mean()
+    MBE_QH_60 = compare_60_QH.difference.mean()
+
+    print(' ')
+    print('MBE')
+    print('QH')
+    print(MBE_QH_1)
+    print(MBE_QH_5)
+    print(MBE_QH_10)
+    print(MBE_QH_60)
+
+    MBE_kdown_1 = compare_1_kdown.difference.mean()
+    MBE_kdown_5 = compare_5_kdown.difference.mean()
+    MBE_kdown_10 = compare_10_kdown.difference.mean()
+    MBE_kdown_60 = compare_60_kdown.difference.mean()
+
+    print('KDOWN')
+    print(MBE_kdown_1)
+    print(MBE_kdown_5)
+    print(MBE_kdown_10)
+    print(MBE_kdown_60)
+
+    # maximum difference
+    print(' ')
+    print('Max diff in kdown 1 min: ',
+          compare_1_kdown.iloc[np.where(compare_1_kdown.abs_diff == compare_1_kdown.abs_diff.max())[0]].difference[0])
+    print('at: ',
+          compare_1_kdown.iloc[np.where(compare_1_kdown.abs_diff == compare_1_kdown.abs_diff.max())[0]].index[0])
+
+    print('Max diff in kdown 5 min: ',
+          compare_5_kdown.iloc[np.where(compare_5_kdown.abs_diff == compare_5_kdown.abs_diff.max())[0]].difference[0])
+    print('at: ',
+          compare_5_kdown.iloc[np.where(compare_5_kdown.abs_diff == compare_5_kdown.abs_diff.max())[0]].index[0])
+
+    print('Max diff in kdown 10 min: ',
+          compare_10_kdown.iloc[np.where(compare_10_kdown.abs_diff == compare_10_kdown.abs_diff.max())[0]].difference[
+              0])
+    print('at: ',
+          compare_10_kdown.iloc[np.where(compare_10_kdown.abs_diff == compare_10_kdown.abs_diff.max())[0]].index[0])
+
+    print('Max diff in kdown 60 min: ',
+          compare_60_kdown.iloc[np.where(compare_60_kdown.abs_diff == compare_60_kdown.abs_diff.max())[0]].difference[
+              0])
+    print('at: ',
+          compare_60_kdown.iloc[np.where(compare_60_kdown.abs_diff == compare_60_kdown.abs_diff.max())[0]].index[0])
+
+    print(' ')
+    print('Max diff in QH 1 min: ',
+          compare_1_QH.iloc[np.where(compare_1_QH.abs_diff == compare_1_QH.abs_diff.max())[0]].difference[0])
+    print('at: ', compare_1_QH.iloc[np.where(compare_1_QH.abs_diff == compare_1_QH.abs_diff.max())[0]].index[0])
+
+    print('Max diff in QH 5 min: ',
+          compare_5_QH.iloc[np.where(compare_5_QH.abs_diff == compare_5_QH.abs_diff.max())[0]].difference[0])
+    print('at: ', compare_5_QH.iloc[np.where(compare_5_QH.abs_diff == compare_5_QH.abs_diff.max())[0]].index[0])
+
+    print('Max diff in QH 10 min: ',
+          compare_10_QH.iloc[np.where(compare_10_QH.abs_diff == compare_10_QH.abs_diff.max())[0]].difference[0])
+    print('at: ', compare_10_QH.iloc[np.where(compare_10_QH.abs_diff == compare_10_QH.abs_diff.max())[0]].index[0])
+
+    print('Max diff in QH 60 min: ',
+          compare_60_QH.iloc[np.where(compare_60_QH.abs_diff == compare_60_QH.abs_diff.max())[0]].difference[0])
+    print('at: ', compare_60_QH.iloc[np.where(compare_60_QH.abs_diff == compare_60_QH.abs_diff.max())[0]].index[0])
+
+    print(' ')
+    print('MAE')
+    print('QH MAE 1 min: ', compare_1_QH.abs_diff.mean())
+    print('QH MAE 5 min: ', compare_5_QH.abs_diff.mean())
+    print('QH MAE 10 min: ', compare_10_QH.abs_diff.mean())
+    print('QH MAE 60 min: ', compare_60_QH.abs_diff.mean())
+
+    print(' ')
+
+    print('KDOWN MAE 1 min: ', compare_1_kdown.abs_diff.mean())
+    print('KDOWN MAE 5 min: ', compare_5_kdown.abs_diff.mean())
+    print('KDOWN MAE 10 min: ', compare_10_kdown.abs_diff.mean())
+    print('KDOWN MAE 60 min: ', compare_60_kdown.abs_diff.mean())
 
     print('end')
