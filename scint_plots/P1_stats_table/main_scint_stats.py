1890e39a1030b18f32add48c5ad0418a4a26b42c
beth182 <bethany.saunders@pgr.reading.ac.uk>
2023-09-28 14:07:04 +0100

    Add % difference steps for centre and weighted average

--- scint_plots/P1_stats_table/main_scint_stats.py
+++ scint_plots/P1_stats_table/main_scint_stats.py
@@ -7,11 +7,10 @@ from scint_plots.P1_stats_table import scint_stat_funs
 from scint_plots.tools.preprocessed_UKV_csvs import UKV_lookup
 from model_eval_tools.retrieve_UKV import retrieve_ukv_vars
 
-
 # main script for running stat functions of P1: Results table
 
 scint_path = 12
-DOY_list = [2016123]
+DOY_list = [2016123, 2016126]
 
 var_list = ['QH', 'kdown', 'z_f']
 
@@ -20,12 +19,16 @@ time_res = '1min_sa10min'
 
 pair_id = look_up.scint_path_numbers[scint_path]
 
+DOY_dict = {}
+
 for DOY in DOY_list:
     # read the observations
     df = read_calculated_fluxes.extract_data(doy_list=[DOY],
                                              pair_id=pair_id,
                                              var_list=var_list,
                                              time_res=time_res)
+
+    DOY_dict[DOY] = {'obs': df}
 
     # stats of observations
     """
@@ -37,15 +40,17 @@ for DOY in DOY_list:
     # """
     # retrieve UKV data
     run_details_H = {'variable': 'H',
-                   'run_time': '21Z',
-                   'scint_path': scint_path,
-                   'grid_number': UKV_lookup.scint_UKV_grid_choices[pair_id][1],  # this is only used if sa_analysis is set to False
-                   'target_height': df.z_f.mean()}
+                     'run_time': '21Z',
+                     'scint_path': scint_path,
+                     'grid_number': UKV_lookup.scint_UKV_grid_choices[pair_id][1],
+                     # this is only used if sa_analysis is set to False
+                     'target_height': df.z_f.mean()}
     run_details_kdown = {'variable': 'kdown',
-                   'run_time': '21Z',
-                   'scint_path': scint_path,
-                   'grid_number': UKV_lookup.scint_UKV_grid_choices[pair_id][1],  # this is only used if sa_analysis is set to False
-                   'target_height': np.nan}
+                         'run_time': '21Z',
+                         'scint_path': scint_path,
+                         'grid_number': UKV_lookup.scint_UKV_grid_choices[pair_id][1],
+                         # this is only used if sa_analysis is set to False
+                         'target_height': np.nan}
 
     # get model sensible heat
     ukv_data_dict_QH = retrieve_ukv_vars.retrieve_UKV(run_choices=run_details_H, DOYstart=DOY, DOYstop=DOY)
@@ -55,6 +60,46 @@ for DOY in DOY_list:
     UKV_df_kdown = retrieve_ukv_vars.UKV_df(ukv_data_dict_kdown)
 
     scint_stat_funs.stats_of_model(df, UKV_df_QH, UKV_df_kdown)
-    print('end')
 
+    DOY_dict[DOY]['UKV_QH'] = UKV_df_QH
+
+    # print('end')
     # """
+
+print('end')
+
+"""
+# constrain times to be just within analysis period
+DOY_dict[2016126]['UKV_QH'] = DOY_dict[2016126]['UKV_QH'].dropna()
+DOY_dict[2016123]['UKV_QH'] = DOY_dict[2016123]['UKV_QH'].dropna()
+
+DOY_dict[2016126]['UKV_QH']['surf_centre_diff'] = ((DOY_dict[2016126]['UKV_QH'][13] - DOY_dict[2016126]['UKV_QH'][
+    'WAverage']) / DOY_dict[2016126]['UKV_QH'][13]) * 100
+DOY_dict[2016123]['UKV_QH']['surf_centre_diff'] = ((DOY_dict[2016123]['UKV_QH'][13] - DOY_dict[2016123]['UKV_QH'][
+    'WAverage']) / DOY_dict[2016123]['UKV_QH'][13]) * 100
+
+# mean percentage difference
+print(' ')
+print('MEAN % DIFFERENCE')
+print(126, ': ', DOY_dict[2016126]['UKV_QH']['surf_centre_diff'].mean())
+print(123, ': ', DOY_dict[2016123]['UKV_QH']['surf_centre_diff'].mean())
+
+# max
+print(' ')
+print('MAX % DIFFERENCE')
+print(126, ': ', DOY_dict[2016126]['UKV_QH']['surf_centre_diff'].iloc[
+    np.where(DOY_dict[2016126]['UKV_QH']['surf_centre_diff'] == DOY_dict[2016126]['UKV_QH']['surf_centre_diff'].max())[
+        0]])
+print(123, ': ', DOY_dict[2016123]['UKV_QH']['surf_centre_diff'].iloc[
+    np.where(DOY_dict[2016123]['UKV_QH']['surf_centre_diff'] == DOY_dict[2016123]['UKV_QH']['surf_centre_diff'].max())[
+        0]])
+# min
+print(' ')
+print('MIN % DIFFERENCE')
+print(126, ': ', DOY_dict[2016126]['UKV_QH']['surf_centre_diff'].iloc[
+    np.where(DOY_dict[2016126]['UKV_QH']['surf_centre_diff'] == DOY_dict[2016126]['UKV_QH']['surf_centre_diff'].min())[
+        0]])
+print(123, ': ', DOY_dict[2016123]['UKV_QH']['surf_centre_diff'].iloc[
+    np.where(DOY_dict[2016123]['UKV_QH']['surf_centre_diff'] == DOY_dict[2016123]['UKV_QH']['surf_centre_diff'].min())[
+        0]])
+"""
