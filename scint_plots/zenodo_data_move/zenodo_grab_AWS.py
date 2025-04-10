960361865778184651323dc5b4e3936044a62e4d
beth182 <bethany.saunders@pgr.reading.ac.uk>
2025-04-10 14:08:52 +0200

    Script copying Davis data to zenodo location

---
+++ scint_plots/zenodo_data_move/zenodo_grab_AWS.py
@@ -0,0 +1,39 @@
+import os
+import pandas as pd
+import shutil
+
+from scint_flux.run_function import construct_path
+from scint_flux.functions.find_files import find_files
+
+bdsm_path = 'D:/Documents/scintools/example_inputs/rasters/height_surface_4m.tif'
+cdsm_path = 'D:/Documents/scintools/example_inputs/rasters/height_veg_4m.tif'
+dem_path = 'D:/Documents/scintools/example_inputs/rasters/height_terrain_4m.tif'
+
+processed_wx_path = '//rdg-home.ad.rdg.ac.uk/research-nfs/rds/micromet/Tier_processing/rv006011/new_data_scint/'
+
+pair_id = 'BCT_IMU'
+
+current_dir = os.getcwd().replace('\\', '/') + '/'
+days_csv_path = current_dir + pair_id + '_days.csv'
+
+assert os.path.isfile(days_csv_path)
+
+days_df = pd.read_csv(days_csv_path)
+
+days_df['YYYYDOY'] = days_df['Year'].astype(str) + days_df['DOY'].astype(str)
+
+path_df = construct_path(pair_id, bdsm_path, cdsm_path, dem_path)
+pair = path_df['pair']
+
+for index, row in days_df.iterrows():
+    doy = row['YYYYDOY']
+
+    wx_files = find_files(site='BCT',
+                          instrument='Davis', level='L2', time_res='1min',
+                          main_dir=processed_wx_path, doy=doy)
+
+    assert len(wx_files['file_paths']) == 1
+    raw_file = wx_files['file_paths'][0]
+
+    shutil.copy(raw_file, 'D:/zenodo/P2 CASES/AWS_processed/')
+    print(raw_file)
